cb796b797dea9fa03711c60a0a67399b94a7af39
MARCELA LUIZA RAMOS REIS <marcela.6966@aluno.pr.senac.br>
2023-04-03 08:12:08 -0300

    exemplo

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+
+echo "1";
+echo"<br>";
+echo "2";
+echo"<br>";
+echo "3";
+echo"<br>";
+echo "4";
+echo"<br>";
+echo "5";
+echo"<br>";
+echo "6";
+echo"<br>";
+echo "7";
+echo"<br>";
+echo "8";
+echo"<br>";
+echo "9";
+echo"<br>";
+echo "10";
+echo"<br>";
+
+for($impar=0; $impar<=100; $impar++){
+    if($impar % 2 == 1 ){
+        echo $impar. " - " ;
+   
+    }
+}
+
+?>
